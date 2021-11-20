ae6ff234e5e573569748aae01a9c36252af209a6
AlysonGCS <alyson.gabriel1707@hotmail.com>
2021-11-20 02:29:49 -0300

    Start Commit

---
+++ app/src/main/java/opet/projetopi/MainActivity.java
@@ -0,0 +1,28 @@
+package opet.projetopi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void intentCad(View view){
+        startActivity(new Intent(this,Register.class));
+    }
+}
